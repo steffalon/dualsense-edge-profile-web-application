ce6bd4ab2e2707ed66ae4d79133aa13e3c6d7803
steffalon <simon.boerrigter@gmail.com>
2023-07-16 21:58:52 +0200

    button assignment screen make buttons visible interactable

--- src/components/Configuration/Assignment.vue
+++ src/components/Configuration/Assignment.vue
@@ -2,14 +2,35 @@
 </script>
 <template>
   <section class="controller-front">
-    <span style="left: 386px; top: 180px" class="circle-button" id="circle"></span>
-    <span style="left: 332px; top: 180px" class="circle-button" id="square"></span>
-    <span style="left: 359px; top: 207px" class="circle-button" id="cross"></span>
-    <span style="left: 359px; top: 155px" class="circle-button" id="triangle"></span>
+    <span style="left: 386px; top: 180px" class="interactable circle-button" id="circle"></span>
+    <span style="left: 332px; top: 180px" class="interactable circle-button" id="square"></span>
+    <span style="left: 359px; top: 207px" class="interactable circle-button" id="cross"></span>
+    <span style="left: 359px; top: 155px" class="interactable circle-button" id="triangle"></span>
 
-    <span style="left: 173px; top: 218px" class="circle-joystick" id="left_joystick"></span>
-    <span style="left: 292px; top: 218px" class="circle-joystick" id="right_joystick"></span>
+    <span style="left: 109px; top: 182px;" class="interactable dpad-button" id="dpad_left"></span>
+    <span style="left: 126.5px; top: 199px; rotate: 270deg" class="interactable dpad-button" id="dpad_bottom"></span>
+    <span style="left: 144px; top: 182px; rotate: 180deg" class="interactable dpad-button" id="dpad_right"></span>
+    <span style="left: 126.5px; top: 166px; rotate: 90deg" class="interactable dpad-button" id="dpad_top"></span>
 
+    <span style="left: 173px; top: 218px" class="interactable circle-joystick" id="left_joystick"></span>
+    <span style="left: 292px; top: 218px" class="interactable circle-joystick" id="right_joystick"></span>
+
+
+    <span style="left: 112px; top: 115px" class="interactable left_1" id="left_1"></span>
+    <span style="left: 348px; top: 115px" class="interactable right_1" id="right_1"></span>
+
+    <span style="left: 104px; top: 64px" class="interactable left_2" id="left_2"></span>
+    <span style="left: 356px; top: 64px" class="interactable right_2" id="right_2"></span>
+
+    <span style="left: 164px; top: 146px" class="interactable create_button" id="create_button"></span>
+    <span style="left: 336px; top: 146px" class="interactable options_button" id="options_button"></span>
+
+    <span style="left: 181px; top: 135px" class="interactable trackpad" id="trackpad"></span>
+
+    <span style="left: 244px; top: 229px" class="interactable circle-button" id="ps_button"></span>
+
+    <span style="left: 198px; top: 310px" class="interactable paddle_left" id="paddle_left"></span>
+    <span style="left: 301px; top: 310px" class="interactable paddle_right" id="paddle_right"></span>
 
     <section class="select-popup">
       <p>Button: []</p>
@@ -41,7 +62,7 @@
 }
 
 .circle-joystick {
-  background-color: yellow;
+  background-color: black;
   width: 47px;
   height: 47px;
   border-radius: 50%;
@@ -56,5 +77,108 @@
   border-radius: 5px;
   box-shadow: 3px 5px 12px 2px rgba(0, 0, 0, 0.15);
   left: 426px; top: 180px
+}
+
+.dpad-button {
+  background-color: black;
+  width: 28px;
+  height: 21px;
+  display: block;
+  position: absolute;
+  clip-path: polygon(0% 0%, 63% 0%, 100% 50%, 63% 100%, 0% 100%);
+  border-radius: 5px;
+}
+
+.left_1 {
+  background-color: black;
+  width: 52px;
+  height: 26px;
+  display: block;
+  border-radius: 6px 6px 20px 20px;
+  position: absolute;
+  rotate: 350deg;
+}
+
+.right_1 {
+  background-color: black;
+  width: 52px;
+  height: 26px;
+  display: block;
+  border-radius: 6px 6px 20px 20px;
+  position: absolute;
+  rotate: 10deg;
+}
+
+.left_2 {
+  background-color: black;
+  width: 52px;
+  height: 46px;
+  display: block;
+  border-radius: 25px 25px 6px 6px;
+  position: absolute;
+  rotate: 350deg;
+}
+
+.right_2 {
+  background-color: black;
+  width: 52px;
+  height: 46px;
+  display: block;
+  border-radius: 25px 25px 6px 6px;
+  position: absolute;
+  rotate: 10deg;
+}
+
+.create_button {
+  background-color: black;
+  width: 12px;
+  height: 22px;
+  display: block;
+  position: absolute;
+  border-radius: 6px;
+  rotate: 350deg;
+}
+
+.options_button {
+  background-color: black;
+  width: 12px;
+  height: 22px;
+  display: block;
+  position: absolute;
+  border-radius: 6px;
+  rotate: 10deg;
+}
+
+.trackpad {
+  background-color: rgba(0,0,0,.7);
+  width: 150px;
+  height: 72px;
+  position: absolute;
+  clip-path: polygon(0 0, 100% 0, 90% 100%, 10% 100%);
+  border-radius: 0 0 35px 35px;
+}
+
+.paddle_left {
+  background-color: black;
+  width: 17px;
+  height: 42px;
+  display: block;
+  position: absolute;
+  border-radius: 8px 8px 35px 8px;
+  rotate: 350deg;
+}
+
+.paddle_right {
+  background-color: black;
+  width: 17px;
+  height: 42px;
+  display: block;
+  position: absolute;
+  border-radius: 8px 8px 8px 35px;
+  rotate: 10deg;
+}
+
+.interactable:hover {
+  background-color: yellow;
 }
 </style>
